d52f11bcb35b9e888d243846d3b384fb0e827031
datawithrocky <datawithrocky@gmail.com>
2026-01-09 12:15:19 +0530

    Create db_config.py

---
+++ db_config.py
@@ -0,0 +1,22 @@
+# db_config.py
+
+db_config = {
+    "mongodb": {
+        "type": "mongodb",
+        "host": "mongodb",
+        "port": 27017,
+        "username": "admin",
+        "password": "admin123",
+        "database": "admin",
+        "source": "test_aws_db"
+    },
+    "postgresql": {
+        "type": "postgresql",
+        "host": "15.164.117.226",
+        "port": 5433,
+        "username": "admin",
+        "password": "admin123",
+        "database": "Ident",
+        "landschm": "public"
+    }
+}
